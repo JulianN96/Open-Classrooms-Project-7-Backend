b2fe26666a7fff383f51aecde4d150fa1694c4d2
Julian Norris <julian.norris96@gmail.com>
2023-10-31 17:37:36 +0100

    Added password and email validity functions

--- controllers/user.js
+++ controllers/user.js
@@ -2,29 +2,53 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const passwordFormatError = 'Mot de passe doit contenir un minimum de 8 charactères, des lettres majuscules et miniuscules, un chiffre et un charactère special'
+const emailFormatError = `Votre addresse mail n'est pas valide`
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const passwordStrengthTester = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+
 exports.signup = (req, res, next) => {
-  //TODO ADD PASSWORD VERIFICATION (No only spaces, not less than 8 characters, complex) AND EMAIL VERIFICATION. Add secruity plugin to stop brute force attacks
-  bcrypt.hash(req.body.password, 10).then(
-    (hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save().then(
-        () => {
-          res.status(201).json({
-            message: 'User added Successfully'
-          });
-        }
-      ).catch(
-        (error)=> {
-          res.status(500).json({
-            error: error
-          })
-        }
-      )
-    }
-  )
+  //Email format checker
+  if(!req.body.email.match(validRegex)){
+    res.status(500).json({
+      error: emailFormatError
+    })
+  } 
+  //Password checker for onlyspaces
+  if(!req.body.password.trim().length){
+    res.status(500).json({
+      error: passwordFormatError
+    })
+  }
+  //Password Strength Checker
+  if(!passwordStrengthTester.test(req.body.password)){
+    res.status(500).json({
+      error: passwordFormatError
+    })
+  }
+  else if(req.body.password.trim().length){
+    bcrypt.hash(req.body.password, 10).then(
+      (hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
+        user.save().then(
+          () => {
+            res.status(201).json({
+              message: 'User added Successfully'
+            });
+          }
+        ).catch(
+          (error)=> {
+            res.status(500).json({
+              error: error
+            })
+          }
+        )
+      }
+    )
+  }
 };
 
 exports.login = (req, res, next) => {
